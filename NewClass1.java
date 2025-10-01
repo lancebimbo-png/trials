fbb068cdcf8bfcfb7b8c4f3212a249eca88df8ea
lancebimbo-png <lancebimbo@g.cjc.edu.ph>
2025-10-01 14:43:58 +0800

    Add files via upload

---
+++ NewClass1.java
@@ -0,0 +1,79 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author Student
+ */
+public class NewClass1 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Select Activity");
+        System.out.println("Multiplication Table Generator");
+        System.out.println("Sum of Natural Numbers");
+        System.out.println("Factorial Calculator");
+        System.out.println("Even and Odd Numbers");
+        System.out.println("Reverse Digits of a Number");
+        int choice = input.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("Enter a Number");
+                int num = input.nextInt();
+                for (int a = 1; a <= 10; a++) {
+                    System.out.println(num + " x " + a + " = " + (num * a));
+                }
+                break;
+
+            case 2:
+                System.out.println("Enter a Number");
+                int b = input.nextInt();
+                int sum = 0;
+                int c = 1;
+                while (c <= b) {
+                    sum = sum + c;
+                    c++;
+                }
+
+                System.out.println("Sum = " + sum);
+                break;
+
+            case 3:
+                System.out.println("Enter a Number");
+                int d = input.nextInt();
+                int factorial = 1;
+                int e = 1;
+                do {
+                    factorial = factorial * e;
+                    e++;
+                } while (e <= d);
+                System.out.println("Factorial = " + factorial);
+                break;
+            
+            case 4:
+                System.out.println("Enter a Number");
+                int f = input.nextInt();
+                System.out.println("Even Numbers");
+                for (int g = 1; g <= f; g++) {
+                    if (g % 2 == 0){
+                    System.out.println(f + "");
+                }
+                }
+                System.out.println("Odd Numbers");
+                for  (int g = 1; g <= f; g++) {
+                    if (g % 2 != 0) {
+                    System.out.println(g + "");
+                }
+                }
+                System.out.println("");
+                break;
+
+        }
+
+    }
+}
