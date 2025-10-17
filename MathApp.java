6631bcc7b9c15db50514d8b9eaf4ca95374f6666
lancebimbo-png <lancebimbo@g.cjc.edu.ph>
2025-10-17 10:25:21 +0800

    MathApp.java

--- MathApp.java
+++ MathApp.java
@@ -1,1 +1,51 @@
+import java.util.Scanner;
+public class MathApp {
+  public static void main (string {} args) {
+  Scanner input = new Scanner(System.in);
+    System.out.println(" Math App Menu ")
+    System.out.println("1. Addition of two numbers");
+    System.out.println("2. Subtraction of two numbers");
+    System.out.println("3. Multiplication of two numbers");
+    System.out.println("4. Division of two numbers");
+    System.out.println("5. Factorial of a number");
+    System.out.println("6. Power (x^y)");
+    System.out.println("7. Sum of numbers 1 to N");
+    System.out.println("8. Check if a number is prime");
+    System.out.println("0. Exit");
+    System.out.println("Enter your choice");
+    choice = input.nextInt();
 
+    switch choice {
+      case 1:
+        System.out.println("Enter First Number");
+        Double add1 = input.nextDouble();
+        System.out.println("Enter Second Number");
+        Double add2 = input.nextDouble();
+        System.out.println("Results" (add1 + add2));
+        break;
+
+      case 2:
+        System.out.println("Enter First Number");
+        Double sub1 = input.nextDouble();
+        System.out.println("Enter Second Number");
+        Double sub2 = input.nextDouble();
+        System.out.println("Results" (sub1 - sub2));
+        break;
+
+      case 3;
+        System.out.println("Enter First Number");
+        Int mul1 = input.nextInt();
+        System.out.println("Enter Second Number");
+        Int mul2 = input.nextInt();
+        System.out.println("Results" (mul1 * mul2));
+        break;
+
+      case 4:
+        
+
+        
+        
+    }
+    
+    }
+}
