eb1043c08dc5a1ffa65fce4fd77287178feb99f6
lancebimbo-png <lancebimbo@g.cjc.edu.ph>
2025-12-02 12:45:09 +0800

    MathApp.java

--- MathApp.java
+++ MathApp.java
@@ -51,79 +51,35 @@ public class MathApp {
         System.out.println("Results:" (div1 / div2));
         }
         break;
-      
-          case 5:
-                    System.out.print("Enter a number: ");
-                    int n = input.nextInt();
-                    if (n < 0) {
-                        System.out.println("Error: Factorial of negative number not possible!");
-                    } else {
-                        long fact = 1;
-                        int i = 1;
-                        while (i <= n) {
-                            fact *= i;
-                            i++;
-                        }
-                        System.out.println("Factorial = " + fact);
-                    }
-                    break;
 
-                case 6:
-                    System.out.print("Enter base (x): ");
-                    double base = input.nextDouble();
-                    System.out.print("Enter exponent (y): ");
-                    double exp = input.nextDouble();
-                    double result = Math.pow(base, exp);
-                    System.out.println("Result: " + result);
-                    break;
+      case 5:
+        System.out.println("Enter a number");
+        int n = input.nextInt();
+        if (n < 0) {
+          System.out.println("Error: Factorial of negative number not possible!");
+        } else }
+          long fact = 1;
+          int i = 1;
+          while (i <= n) {
+            fact *= i;
+            i++;
+          }
+        break;
 
-                case 7:
-                    System.out.print("Enter N: ");
-                    int num = input.nextInt();
-                    int sum = 0;
-                    for (int j = 1; j <= num; j++) {
-                        sum += j;
-                    }
-                    System.out.println("Sum of numbers 1 to " + num + " = " + sum);
-                    break;
+      case 6:
+        System.out.println("Enter base (x): ");
+        double base = input.nextDouble();
+        System.out.print("Enter exponent (y): ");
+        double exp = input.nextDouble();
+        double result = Math.pow(base, exp);
+        System.out.println("Result: " + result);
+        break;
 
-                case 8:
-                    System.out.print("Enter a number: ");
-                    int primeNum = input.nextInt();
-                    if (primeNum <= 1) {
-                        System.out.println(primeNum + " is NOT a prime number.");
-                    } else {
-                        boolean isPrime = true;
-                        for (int k = 2; k <= primeNum / 2; k++) {
-                            if (primeNum % k == 0) {
-                                isPrime = false;
-                                break;
-                            }
-                        }
-                        if (isPrime) {
-                            System.out.println(primeNum + " is a prime number.");
-                        } else {
-                            System.out.println(primeNum + " is NOT a prime number.");
-                        }
-                    }
-                    break;
+    case 7:
+      System.out.println("")
 
-                case 0:
-                    System.out.println("Thank you for using Math App!");
-                    System.exit(0);
-                    break;
+    
+        
 
-                default:
-                    System.out.println("Invalid choice! Please try again.");
-            }
-
-            System.out.print("Do you want to perform another computation? (yes/no): ");
-            again = input.next();
-
-        } while (again.equalsIgnoreCase("yes"));
-
-        System.out.println("Thank you for using Math App!");
-        input.close();
-    }
-}
-}
+        
+         
